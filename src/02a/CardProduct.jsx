47f4c5b32bb98bad7c3da21a24cefa5fdc3a0ef8
muhsadno <1526050+msadno@users.noreply.github.com>
2025-05-05 16:42:11 +0800

    "add: css"

--- src/02a/CardProduct.jsx
+++ src/02a/CardProduct.jsx
@@ -1,6 +1,6 @@
 const CardProduct = ({ title, price, description, image }) => {
   return (
-    <div className="flex flex-col justify-center w-[300px] bg-amber-200 p-3 rounded-md">
+    <div className="flex flex-col justify-center w-[300px] bg-gradient-to-br from-amber-300 to-amber-200  p-3 rounded-md">
       <img
         className="w-full h-[300px] transition-all duration-700 ease-in-out hover:scale-110 object-scale-down rounded-md"
         src={image}
