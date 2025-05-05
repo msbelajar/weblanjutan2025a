4e29f7c2b28fb26ea911f2adc518dd44537dd531
muhsadno <1526050+msadno@users.noreply.github.com>
2025-05-05 15:47:26 +0800

    "add: commerce"

--- src/02a/App.jsx
+++ src/02a/App.jsx
@@ -1,14 +1,22 @@
-import { products } from "./products"
-import CardProduct from "./CardProduct"
+import { products } from "./products";
+import CardProduct from "./CardProduct";
 
 const App = () => {
   return (
-    <div className="flex flex-wrap justify-between gap-2">
-        {products.map( (product) => (
-            <CardProduct key={product.id} {...product} />
+    <div>
+      <div className="flex items-center justify-center h-[150px] bg-gradient-to-br from-blue-600 to-blue-300 mb-4">
+        <h1 className="text-5xl">Selamat Datang</h1>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4">
+        {products.map((product) => (
+          <CardProduct key={product.id} {...product} />
         ))}
+      </div>
+      <div className="flex items-center justify-center h-[50px] bg-gradient-to-tl from-blue-600 to-blue-300 my-2">
+        <h1 className="text-md">@2025</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/02a/CardProduct.jsx
+++ src/02a/CardProduct.jsx
@@ -1,12 +1,13 @@
 const CardProduct = ({ title, price, description, image }) => {
   return (
-    <div className="w-[300px] bg-green-300 p-3 rounded-md">
-      <div className="transition-all duration-1000 ease-in-out hover:scale-125">
-        <img className="rounded-md" src={image} />
-      </div>
+    <div className="flex flex-col justify-center w-[300px] bg-amber-200 p-3 rounded-md">
+      <img
+        className="w-full h-[300px] transition-all duration-700 ease-in-out hover:scale-110 object-scale-down rounded-md"
+        src={image}
+      />
       <h1 className="font-bold text-xl">{title}</h1>
       <p className="line-clamp-3">{description}</p>
-      <h1>$ {price}</h1>
+      <h1 className="text-md font-bold">$ {price}</h1>
     </div>
   );
 };
