4219d1664025c4696f29f3a1299dec08090a1949
muhsadno <1526050+msadno@users.noreply.github.com>
2025-05-28 10:10:59 +0800

    "update"

--- src/05b/App.jsx
+++ src/05b/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <div>
         <BrowserRouter>
             <Routes>
-                <Route />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
     </div>
